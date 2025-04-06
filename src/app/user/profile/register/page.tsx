dead4e8851bc5149f0cda8aa46cb46542937d267
Kazuya-Sakashita <kz0508@gmail.com>
2025-04-06 16:00:27 +0900

    修正予定コメント追加

--- src/app/user/profile/register/page.tsx
+++ src/app/user/profile/register/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+// TODO リファクタリング必要
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
